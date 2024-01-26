846e7b75b2e17e9f46781ec8b00ac98340fe248d
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-26 18:20:07 +0100

    completed milestone 1

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php 
 
-    function generateRandomString($length = 10) {
-        // variabile dei characters
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-+.,';
-        $charactersLength = strlen($characters);
-        $randomString = '';
+    if(isset($_GET['psw_length']) && $_GET['psw_length'] != ''){
+        
+        function generateRandomString($length) {
+            // variabile dei characters
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-+.,';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+    
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[random_int(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        };
+    
+    }
 
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[random_int(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    };
-
-    echo generateRandomString();
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +28,10 @@
 <body>
     <!-- creo la form -->
     <form action="./index.php" method="GET">
-        <input type="number" id="tentacles" name="psw_lenght" min="7" max="20" />
+        <input type="number" id="tentacles" name="psw_length" min="1" max="25" />
     </form>
+    <div>
+        <?php echo generateRandomString($_GET['psw_length']) ?>
+    </div>
 </body>
 </html>
