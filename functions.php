b85ce23fc1a0a1544bf99a9a05b14daf4e40e47b
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-29 10:52:34 +0100

    added comments in functions.php

--- functions.php
+++ functions.php
@@ -6,11 +6,15 @@
         // Variabile dei characters
         $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?~@#-+.,';
 
+        // Recupero la lunghezza della variabile
         $charactersLength = strlen($characters);
+
+        // Variabile che conterrà la password generata
         $randomString = '';
 
+        // Condizione if, se viene inserita una password minore di 7 caratteri o maggiore di 30 restituisco una stringa, altrimenti genero la password
         if ($length < 7 || $length > 30) {
-            $randomString = 'La lunghezza della password deve essere tra i 7 ed i 30 caratteri';
+            $randomString = 'La lunghezza della password deve essere tra i 7 ed i 30 caratteri!';
         }
         else{
             for ($i = 0; $i < $length; $i++) {
